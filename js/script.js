1e49a1d5710ec12b3774a975856c5988f8513f1b
MrArcano <salvatore-dario@hotmail.it>
2023-10-02 12:06:41 +0200

    [init]

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+const { createApp } = Vue;
+
+createApp({
+  mounted() {
+    console.log("Montato");
+  },
+
+}).mount("#app")
